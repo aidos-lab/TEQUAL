063f5aa23471f4d817499e076ad2c14b3e205f0f
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-09-18 18:34:24 +0200

    sorting experiment files

--- src/scripts/trainer.sh
+++ src/scripts/trainer.sh
@@ -8,10 +8,11 @@ if [ -n "$params" ]; then
 EXPERIMENT="$(cat $params | shyaml get-value experiment)"
 fi
 
-CONFIGS="$root/src/experiments/$EXPERIMENT/configs/*"
+# CONFIGS="$root/experiments/$EXPERIMENT/configs/*"
+CONFIGS=`ls $root/experiments/$EXPERIMENT/configs/config_* | sort -V`
 
 cd "$root/src"
 for c in $CONFIGS
 do
-	poetry run python train.py -c $c
+    poetry run python train.py -c $c
 done
